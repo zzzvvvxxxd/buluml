9c601d510764c5ab3dffda60020bd5740d56797a
zzzvvvxxxd <zzzvvvxxxd@163.com>
2015-10-08 19:11:20 +0800

    add Dictionary

---
+++ build/lib/buluml/TopicModel.py
@@ -0,0 +1,25 @@
+# encoding=utf-8
+__author__ = 'LoveLYJ'
+
+
+class pLSA():
+    def __init__(self):
+        pass
+
+    def fit(self):
+        """
+        训练pLSA
+        [
+            [token1, token2],
+            [token1, token2, token3, token4]
+        ]
+        :return:
+        """
+        pass
+
+    def transform(self):
+        # no use
+        pass
+
+if __name__ == "__main__":
+    pass
--- buluml/toolkit.py
+++
@@ -1,7 +0,0 @@
-# encoding=utf-8
-__author__ = 'LoveLYJ'
-import pandas as pd
-from numpy import loadtxt
-
-def load_data_as_dataFrame(path, sep=','):
-    return pd.read_table(path, sep, header = None)
